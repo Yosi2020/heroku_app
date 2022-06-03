29c65112639b7a290af22bd4ca1027cac9ab57df
Eyosiyas Tibebu <70751963+Yosi2020@users.noreply.github.com>
2022-06-03 23:30:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 model = joblib.load(open('decision_tree_model'))
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/predict', methods = ['POST'])
 
 def predict():
 	event = json.loads(request.data)
